820e8d79c76ff164e0e8f7ba81aa06515e2d562b
Malek Arafat <arafatmalek44@gmail.com>
2024-12-13 12:29:20 +0530

    changes

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get("/",(req,res)=>{
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
-    res.locals.CU=req.user;
+    res.locals.currentUser=req.user;
     next();
 })
 
